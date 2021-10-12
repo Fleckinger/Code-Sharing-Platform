9cf3d345f04710610c942dcd98d7a1902064f974
Vladislav Safarov <egbertz@yandex.ru>
2021-10-12 13:53:39 +0300

    add getCode() method to apiController, method just print TestGetCode

--- src/main/java/com/fleckinger/codesharingplatform/controller/ApiController.java
+++ src/main/java/com/fleckinger/codesharingplatform/controller/ApiController.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class ApiController {
 
     @GetMapping("/api/code")
+    public void getCode() {
+        System.out.println("TestGetCode");
+    }
 
 
 }
