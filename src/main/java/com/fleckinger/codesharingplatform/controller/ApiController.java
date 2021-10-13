01c9f77faf6e8fc2c8b00b39b4a66458330c8655
Vladislav Safarov <egbertz@yandex.ru>
2021-10-13 11:38:46 +0300

    add /code endpoint in webController and api/code endpoint in apiController. Pass first stage tests.

--- src/main/java/com/fleckinger/codesharingplatform/controller/ApiController.java
+++ src/main/java/com/fleckinger/codesharingplatform/controller/ApiController.java
@@ -1,16 +1,24 @@
 package com.fleckinger.codesharingplatform.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.Map;
 
 
 @Controller
 public class ApiController {
 
-    @GetMapping("/api/code")
-    public void getCode() {
-        System.out.println("TestGetCode");
+    @RequestMapping(value = "/api/code", method = RequestMethod.GET)
+    @ResponseBody
+    public Map<String, String> getCode(HttpServletResponse response) {
+        response.addHeader("Content-Type", "text/html");
+        Map<String, String> responseMap = new HashMap<>();
+        responseMap.put("code", "test");
+        return responseMap;
     }
-
-
 }
--- src/main/java/com/fleckinger/codesharingplatform/controller/WebController.java
+++ src/main/java/com/fleckinger/codesharingplatform/controller/WebController.java
@@ -1,5 +1,27 @@
 package com.fleckinger.codesharingplatform.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+@Controller
 public class WebController {
+
+    /**
+     *
+     * @return getCode page.html from static folder
+     */
+    @RequestMapping(value = "/code", method = RequestMethod.GET)
+    public String getCode() {
+
+        /*return "<html> " +
+                "<head> " +
+                "<title>Code</title> " +
+                "</head> " +
+                "<body>";*/
+        return "getCode page.html";
+    }
+
 
 }
